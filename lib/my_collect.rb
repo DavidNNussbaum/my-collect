6c8032a84fc22716c1ebc5148500ad2a5ab672ef
DavidNNussbaum <brocknynj@aol.com>
2020-10-29 10:25:04 -0400

    commit tree c5a723560657c964b679f99ac7d70105de68e59f

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,21 @@
+def my_collect(collection)
+    i = 0
+    collection1 = []
+    while i < collection.length
+        collection1 << yield(collection[i])
+    i += 1
+    end
+    collection1
+end
 
 
+
+# def hello(array)
+#   i = 0
+#   collection = []
+#   while i < array.length
+#     collection << yield(array[i])
+#     i += 1
+#   end
+#   collection
+# end 
